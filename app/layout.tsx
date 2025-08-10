bd8566f5e9a7cae68b1687c92c3af34bd765ba17
Ravi Kant <ravikant@Ravis-MacBook-Air.local>
2025-08-10 12:20:05 +0530

    remove unnessary  function

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import "./globals.css";
 
-import { GoogleAnalytics } from "@next/third-parties/google";
+
 import { Inter as FontSans } from "next/font/google";
 import localFont from "next/font/local";
 
-import { Analytics } from "@/components/common/analytics";
+
 import { ThemeProvider } from "@/components/common/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
 import { siteConfig } from "@/config/site";
@@ -96,10 +96,7 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }: RootLayoutProps) {
-  const GA_ID = process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID;
-  if (!GA_ID) {
-    throw new Error("Missing Google Analytics ID");
-  }
+  
 
   return (
     <html lang="en" suppressHydrationWarning>
@@ -126,12 +123,12 @@ export default function RootLayout({ children }: RootLayoutProps) {
           ]}
         >
           {children}
-          <Analytics />
+          
           <Toaster />
           <ModalProvider />
         </ThemeProvider>
       </body>
-      <GoogleAnalytics gaId={GA_ID} />
+      
     </html>
   );
 }
