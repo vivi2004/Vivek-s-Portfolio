d48b71b9dbf36b8e175ad63c7b1e9742878c1934
vivi2004 <vivekpurbey586@gmail.com>
2026-01-14 01:15:48 +0530

    Update resume link in resume.ts

--- config/resume.ts
+++ config/resume.ts
@@ -1,3 +1,3 @@
 export const resumeLink =
   process.env.NEXT_PUBLIC_RESUME_LINK ||
-  "https://drive.google.com/file/d/1z4Mq1yEOeb6YtovQEJXGa4Op_7KLuPct/view?usp=sharing";
+  "https://drive.google.com/file/d/1kzLiwVUR4ckAmb0VxgRiSqgqNb_ao1Hu/view?usp=sharing";
